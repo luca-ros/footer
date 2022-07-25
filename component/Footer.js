bb2249432cc8430673e92074ac93138c317fe362
luca-ros <lucattparte@gmail.com>
2022-07-25 14:09:12 +0200

    style: footer icons space-around  & flex wrap

--- component/Footer.js
+++ component/Footer.js
@@ -9,15 +9,15 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.social_icon}>
-        <a href="https://www.youtube.com/watch?v=UpkEANWC2Ms&t=354s" target="_blank"><FaInstagram className={FaInstagram} /></a>
+        <a href="#" target="_blank"><FaInstagram className={FaInstagram} /></a>
         <a><FaTwitter className={FaTwitter} /></a>
         <a><FaLinkedin className={FaLinkedin} /></a>
-        <a><FaYoutube className={FaYoutube} /></a>
+        <a href="https://www.youtube.com/watch?v=UpkEANWC2Ms&t" target="_blank"><FaYoutube className={FaYoutube} /></a>
         <a><FaWhatsapp className={FaWhatsapp} /></a>
         <a><FaFacebook className={FaFacebook} /></a>
       </div>
         
-      <p>	&#169;2022 All rights Reserved | Silvana's Mango S.r.l | P.Iva 00000000000 | </p>
+      <p>	&#169;2022 All rights Reserved | Luca Rosano S.r.l | P.Iva 00000000000 | </p>
     </footer>
   );
 };
